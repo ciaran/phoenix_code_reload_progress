a981ecb917eb920265e6e68f384dbb1de15a1b49
Ciarán Walsh <git@ciaranwal.sh>
2016-11-28 19:21:52 +0000

    Shared header template

--- lib/phoenix_code_reload_progress/code_reloader.ex
+++ lib/phoenix_code_reload_progress/code_reloader.ex
@@ -102,12 +102,12 @@ defmodule PhoenixCodeReloadProgress.CodeReloader do
     res
   end
 
-  defp template(conn, output) do
+  defp header_template(title) do
     """
     <!DOCTYPE html>
     <html>
     <head>
-        <title>CompilationError at #{method(conn)} #{conn.request_path}</title>
+        <title>#{title}</title>
         <style>
         * {
             margin: 0;
@@ -207,6 +207,12 @@ defmodule PhoenixCodeReloadProgress.CodeReloader do
         </style>
     </head>
     <body>
+    """
+  end
+
+  defp template(conn, output) do
+    header_template("CompilationError at #{method(conn)} #{conn.request_path}") <>
+    """
         <div class="top">
             <header class="exception">
                 <h2><strong>CompilationError</strong> <span>at #{method(conn)} #{conn.request_path}</span></h2>
@@ -225,120 +231,18 @@ defmodule PhoenixCodeReloadProgress.CodeReloader do
   end
 
   defp compile_template do
+    header_template("Compilation Output") <>
     """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Compilation Output</title>
-        <style>
-        * {
-            margin: 0;
-            padding: 0;
-        }
+        <div id="compile-output">
+            <div class="top">
+                <header class="exception">
+                    <h2>Compilation Output</h2>
+                    <p>Showing console output</p>
+                </header>
+            </div>
 
-        body {
-            font-size: 10pt;
-            font-family: helvetica neue, lucida grande, sans-serif;
-            line-height: 1.5;
-            color: #333;
-            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.6);
-        }
-
-        html {
-            background: #f0f0f5;
-        }
-
-        header.exception {
-            padding: 18px 20px;
-
-            height: 59px;
-            min-height: 59px;
-
-            overflow: hidden;
-
-            background-color: #20202a;
-            color: #aaa;
-            text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3);
-            font-weight: 200;
-            box-shadow: inset 0 -5px 3px -3px rgba(0, 0, 0, 0.05), inset 0 -1px 0 rgba(0, 0, 0, 0.05);
-
-            -webkit-text-smoothing: antialiased;
-        }
-
-        header.exception h2 {
-            font-weight: 200;
-            font-size: 11pt;
-            padding-bottom: 2pt;
-        }
-
-        header.exception h2,
-        header.exception p {
-            line-height: 1.4em;
-            height: 1.4em;
-            overflow: hidden;
-            white-space: pre;
-            text-overflow: ellipsis;
-        }
-
-        header.exception h2 strong {
-            font-weight: 700;
-            color: #7E5ABE;
-        }
-
-        header.exception p {
-            font-weight: 200;
-            font-size: 18pt;
-            color: white;
-        }
-
-        pre, code {
-            font-family: menlo, lucida console, monospace;
-            font-size: 9pt;
-        }
-
-        .trace_info {
-            margin: 10px;
-            background: #fff;
-            padding: 6px;
-            border-radius: 3px;
-            margin-bottom: 2px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.03), 1px 1px 0 rgba(0, 0, 0, 0.05), -1px 1px 0 rgba(0, 0, 0, 0.05), 0 0 0 4px rgba(0, 0, 0, 0.04);
-        }
-
-        .code {
-            background: #fff;
-            box-shadow: inset 3px 3px 3px rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(0, 0, 0, 0.1);
-            margin-bottom: -1px;
-            padding: 10px;
-            overflow: auto;
-        }
-
-        .code::-webkit-scrollbar {
-            width: 10px;
-            height: 10px;
-        }
-
-        .code::-webkit-scrollbar-thumb {
-            background: #ccc;
-            border-radius: 5px;
-        }
-
-        .code:hover::-webkit-scrollbar-thumb {
-            background: #888;
-        }
-        </style>
-    </head>
-    <body>
-        <div class="top">
-            <header class="exception">
-                <h2>Compilaiton Output</h2>
-                <p>Showing console output</p>
-            </header>
-        </div>
-
-        <header class="trace_info">
-            <div class="code">
-    <pre>
+            <header class="trace_info">
+                <div class="code"><pre>
     """
   end
 
